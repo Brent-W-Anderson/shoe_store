c2b583037ec8ed61502987cfcaf0261fc3b80313
gobrent21@gmail.com <gobrent21@gmail.com>
2022-02-03 16:01:02 -0600

    feat: assignment 3 complete

--- src/components/order/order.php
+++ src/components/order/order.php
@@ -15,40 +15,132 @@
 
 <?php
     $selected = $_GET['selected'];
-    $qty = 1;
 
     if( $_SERVER['REQUEST_METHOD'] === 'POST' ) {
+        // variables
         $shoeN = $_POST['shoes'];
         $qty = $_POST['qty'];
-        $delivery = $_POST['delivery'];
         $price = $_POST['price'];
+        $fName = $_POST['fName'];
+        $lName = $_POST['lName'];
+        $deliveryMethod = $_POST['delivery'];
 
+        // dates
+        date_default_timezone_set('America/Chicago'); // central timezone.
+        $date = date( 'm/d/y' );
+        $pickupDate = date( 'm/d/y', strtotime( '+3 day' ) );
+
+        // total price calculations with taxes (10%).
         $amount = $price * $qty;
         $flatRate = 5;
         $tax = .1 * $amount;
 
         $total = $amount + $tax + $flatRate;
 
-        $date = date( 'd/m/y' );
-        $pickup = date( 'd/m/y', strtotime( '+3 day' ) );
+        // discounts on amount:
+        $discount = 0;
+        $shoeOrShoes = 'shoes';
+        switch( $qty ) {
+          case 1: { // only 1 shoe
+            $shoeOrShoes = 'shoe';
+          } break;
+          case ( $qty <= '4' ): { // 2 - 4 shoes
+            $discount = $total * .1;
+            $total -= $discount;
+          } break;
+          default: { // 5 or more shoes
+            $discount = $total * .25;
+            $total -= $discount;
+          }
+        };
 
-        echo 
-        '<div class="confirmation">
-          <div class="box">
-            <div class="header">
-              <h2> PURCHASE COMPLETE </h2>
+        // shipping fee:
+        $shippingFee = 0;
+        if( $total < '50' && $deliveryMethod === 'delivery' ) {
+          $shippingFee = 6.95;
+          $total += $shippingFee;
+        }
+        else {
+          $discount += 6.95;
+        }
+
+        if( $deliveryMethod === 'delivery' ) {// deliveries need addresses with associated data
+          $street = $_POST['street'];
+          $city = $_POST['city'];
+          $state = $_POST['state'];
+          $zip = $_POST['zip'];
+          
+          echo 
+          '<div class="confirmation">
+            <div class="box">
+              <div class="header">
+                <h2> PURCHASE COMPLETE </h2>
+              </div>
+  
+              <p> '.$fName.' '.$lName.', you have ordered '.$qty.' '.$shoeN.' '.$shoeOrShoes.' </p>
+              <p> Placed on: '.$date.' </p>
+              <p> Expected '.$deliveryMethod.': '.$pickupDate.' </p>
+              <br />
+              <p> '.$street.' </p>
+              <p> '.$city.', '.$state.' '.$zip.' </p>
+              <br />
+              <p> Total: $'.number_format( $total, 2 ).' </p>
+              <p> Saved: $'.number_format( $discount, 2 ).' </p>
+              <a class="close" href="./order.php?selected='.$selected.'&qty='.$qty.'&fName='.$fName.'&lName='.$lName.'&delivery='.$deliveryMethod.'&street='.$street.'&city='.$city.'&state='.$state.'&zip='.$zip.'"> Close </a>
             </div>
+          </div>';
+        }
+        else { // pick up orders won't need an address or any association
+          echo 
+          '<div class="confirmation">
+            <div class="box">
+              <div class="header">
+                <h2> PURCHASE COMPLETE </h2>
+              </div>
+  
+              <p> '.$fName.' '.$lName.', you have ordered '.$qty.' '.$shoeN.' '.$shoeOrShoes.' </p>
+              <p> Placed on: '.$date.' </p>
+              <p> Expected '.$deliveryMethod.': '.$pickupDate.' </p>
+              <br />
+              <p> Total: $'.number_format( $total, 2 ).' </p>
+              <p> Saved: $'.number_format( $discount, 2 ).' </p>
+              <a class="close" href="./order.php?selected='.$selected.'&qty='.$qty.'&fName='.$fName.'&lName='.$lName.'&delivery='.$deliveryMethod.'"> Close </a>
+            </div>
+          </div>';
+        }
 
-            <p> shoe: '.$shoeN.' </p>
-            <p> quantity: '.$qty.' </p>
-            <br />
-            <p> order placed: '.$date.' </p>
-            <p> pickup date: '.$pickup.' </p>
-            <p> delivery: '.$delivery.' </p>
-            <br />
-            <p> total: $'.round( $total, 2 ).' </p>
-            <a class="close" href="./order.php?selected='.$selected.'&qty='.$qty.'&delivery='.$delivery.'"> Close </a>
-          </div>
-        </div>';
+        // open file
+        $file = 'orders.json';
+        $current = file_get_contents( $file );
+
+        // define the data we want to save to file
+        $order = new stdClass();
+        $order -> date = date( 'm-d-y' ); // just 1 \, but 2 are needed to breakout
+        $order -> fullName = $fName." ".$lName;
+
+        if( $deliveryMethod === 'delivery' ) {// deliveries need addresses with associated data
+          $order -> address = $street." ".$city.", ".$state." ".$zip;
+        }
+
+        $order -> totalPaid = round( $total, 2 );
+
+        // convert object into a json string
+        $orderData = json_encode( $order );
+
+        // append to previous data
+        if( $current ) { // if previous data exists
+          // open up the array
+          $current = trim( $current, '[' );
+          $current = trim( $current, ']' );
+
+          // so we can append a new order object
+          $current .= ",\n".$orderData;
+        }
+        else {
+          $current .= $orderData;
+        }
+
+        // save that data to file within a new array
+        file_put_contents( $file, "[".$current."]" );
     }
 ?>
--- src/components/order/order.tsx
+++ src/components/order/order.tsx
@@ -23,7 +23,13 @@ export default class Order extends Component {
     },
     hideShadow: false,
     qty: 1,
-    delivery: 'pick up'
+    fName: '',
+    lName: '',
+    delivery: 'delivery',
+    street: '',
+    city: '',
+    state: '',
+    zip: ''
   }
 
   contentDisplayRef = React.createRef<HTMLDivElement>();
@@ -41,26 +47,50 @@ export default class Order extends Component {
     let selected = url.searchParams.get( 'selected' );
     let qty = url.searchParams.get( 'qty' );
     let delivery = url.searchParams.get( 'delivery' );
+    let fName = url.searchParams.get( 'fName' );
+    let lName = url.searchParams.get( 'lName' );
+    let street = url.searchParams.get( 'street' );
+    let city = url.searchParams.get( 'city' );
+    let state = url.searchParams.get( 'state' );
+    let zip = url.searchParams.get( 'zip' );
 
     // if not, then let's define our own defaults.
-    selected ? null : selected = '0';
+    selected ? null : selected = this.state.selected.toString();
     const card = menu[ parseInt( selected ) ];
 
-    qty ? null : qty = '1';
+    qty ? null : qty = this.state.qty.toString();
     const x = parseInt( qty );
 
-    delivery ? null : delivery = 'pick up';
+    fName ? null : fName = this.state.fName;
+    
+    lName ? null : lName = this.state.lName;
+
+    delivery ? null : delivery = this.state.delivery;
+    
+    street ? null : street = this.state.street;
+
+    city ? null : city = this.state.city;
+
+    state ? null : state = this.state.state;
+
+    zip ? null : zip = this.state.zip;
 
     // set the state:
     this.setState( {
-      selected: selected,
+      selected: parseInt( selected ),
       card: {
         asset: card.asset,
         name: card.name,
         price: card.price
       },
       qty: x,
-      delivery: delivery
+      fName: fName,
+      lName: lName,
+      delivery: delivery,
+      street: street,
+      city: city,
+      state: state,
+      zip: zip
     } );
   }
 
@@ -68,7 +98,7 @@ export default class Order extends Component {
     const display = this.contentDisplayRef.current;
 
     if( display ) { // manages if there's a card shadow or not.
-      display.clientWidth === window.innerWidth ?
+      display.clientWidth >= window.innerWidth ?
         this.setState({ hideShadow: true }) : this.setState({ hideShadow: false });
     }
   }
@@ -104,19 +134,36 @@ export default class Order extends Component {
     } );
   }
 
-  handleQty = ( e:React.ChangeEvent<HTMLInputElement> ) => {
+  selectQty = ( e:React.ChangeEvent<HTMLInputElement> ) => {
     const x = parseInt( e.target.value );
 
-    if( x >= 1 ) {
+    if( x > 10 ) { // upper limit
+      this.setState({
+        qty: 10
+      });
+    }
+    else if( x >= 1 ) {
       this.setState({
         qty: x
       });
     }
-    else {
+    else { // lower limit
       this.setState({
         qty: 1
       });
     }
+  }
+
+  selectFName = ( e:React.ChangeEvent<HTMLInputElement> ) => {
+    this.setState({
+      fName: e.target.value
+    });
+  }
+
+  selectLName = ( e:React.ChangeEvent<HTMLInputElement> ) => {
+    this.setState({
+      lName: e.target.value
+    });
   }
 
   selectDelivery = ( e:React.ChangeEvent<HTMLSelectElement> ) => {
@@ -125,33 +172,125 @@ export default class Order extends Component {
     });
   }
 
+  selectStreet = ( e:React.ChangeEvent<HTMLInputElement> ) => {
+    this.setState({
+      street: e.target.value
+    });
+  }
+
+  selectCity = ( e:React.ChangeEvent<HTMLInputElement> ) => {
+    this.setState({
+      city: e.target.value
+    });
+  }
+
+  selectState = ( e:React.ChangeEvent<HTMLInputElement> ) => {
+    this.setState({
+      state: e.target.value
+    });
+  }
+
+  selectZip = ( e:React.ChangeEvent<HTMLInputElement> ) => {
+    this.setState({
+      zip: e.target.value
+    });
+  }
+
+  handleDelivery = () => {
+    const { delivery, street, city, state, zip } = this.state;
+
+    if( delivery === "delivery" ) {
+      return (
+        <div className="address" >
+          <label htmlFor="address">Address:</label>
+
+          <input 
+            type="text" 
+            placeholder="Street" 
+            name="street" 
+            maxLength={ 255 }
+            title="Valid street name" 
+            value={ street } 
+            onChange={ this.selectStreet } 
+            required 
+          />
+          <input 
+            type="text" 
+            placeholder="City" 
+            name="city" 
+            maxLength={ 255 } 
+            title="Valid city name" 
+            value={ city }
+            onChange={ this.selectCity } 
+            required 
+          />
+          <input 
+            type="text" 
+            placeholder="State" 
+            name="state" 
+            pattern="[A-Za-z]{2}" 
+            maxLength={ 2 } 
+            minLength={ 2 } 
+            title="Two letter country code" 
+            value={ state }
+            onChange={ this.selectState } 
+            required 
+          />
+          <input 
+            type="text" 
+            placeholder="Zip" 
+            name="zip" 
+            pattern="\d{5}" 
+            maxLength={ 5 } 
+            minLength={ 5 } 
+            title="Five digit zip code" 
+            value={ zip }
+            onChange={ this.selectZip } 
+            required 
+          />
+        </div>
+      );
+    }
+
+    return null;
+  }
+
   render() {
-    const { selected, card, hideShadow, qty, delivery } = this.state;
+    const { selected, card, hideShadow, qty, fName, lName, delivery, street, city, state, zip } = this.state;
 
     return (
       <div 
         className="content_display" 
         ref={ this.contentDisplayRef } 
         style={ { 
-          boxShadow: hideShadow ? 'none' : '0 max( 8px, 2vh ) max( 12px, 3vh ) rgba( 0, 0, 0, .3 )' 
+          boxShadow: hideShadow ? 'none' : '0 max( 8px, min( 2vh, 16px ) ) max( 12px, min( 3vh, 24px ) ) rgba( 0, 0, 0, .3 )' 
         } }
       >
         <div className="content_left">
-          <form action={ `./order.php?selected=${ selected }&qty=${ qty }&delivery=${ delivery }` } method="POST">
+          <form action={ `./order.php?selected=${ selected }&qty=${ qty }&fName=${ fName }&lName=${ lName }&delivery=${ delivery }&street=${ street }&city=${ city }&state=${ state }&zip=${ zip }` } method="POST">
             <label htmlFor="shoes"> Pick your shoe: </label>
             <select name="shoes" value={ card.name } onChange={ this.selectOption }>
               { menu.map( ( shoe, idx ) => { return this.handleOptions( shoe, idx ) } ) }
             </select>
 
             <label htmlFor="qty"> Quantity: </label>
-            <input name="qty" type="number" value={ qty } onChange={ this.handleQty } />
+            <input name="qty" type="number" min="1" max="10" value={ qty } onChange={ this.selectQty } title="A Value 1 - 10" required />
 
+            <label htmlFor="fName"> First Name: </label>
+            <input type="text" name="fName" maxLength={ 255 } value={ fName } onChange={ this.selectFName } title="Valid first name" required />
+
+            <label htmlFor="lName"> Last Name: </label>
+            <input type="text" name="lName" maxLength={ 255 } value={ lName } onChange={ this.selectLName } title="Valid last name" required />
+
+            <label htmlFor="delivery"> Delivery: </label>
             <select value={ delivery } onChange={ this.selectDelivery } name="delivery">
               <option value="pick up"> pick up </option>
-              <option value="ship"> ship </option>
+              <option value="delivery"> ship </option>
             </select>
 
-            <input name="price" style={ { display: 'none' } } value={ card.price } readOnly />
+            { this.handleDelivery() }
+
+            <input name="price" type="number" style={ { display: 'none' } } value={ card.price } readOnly />
 
             <button>Purchase</button>
           </form>
