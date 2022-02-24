5239ba15f07d780e6d24c1de4315f945396e27fd
gobrent21@gmail.com <gobrent21@gmail.com>
2022-02-23 19:44:15 -0600

    more adjusting

--- src/index.php
+++ src/index.php
@@ -1,5 +1,3 @@
-<!DOCTYPE html>
-
 <?php
   // open file
   $file = './reviews.json';
@@ -7,18 +5,18 @@
   // append to previous data
   if( file_exists( $file ) ) { // if previous data exists
     $current = file_get_contents( $file );
-    $current = json_decode( $current );
 
     $current = str_replace( array( 'ghastly', 'repulsive', 'appalling', 'horrendous' ), '#bad', $current );
 
-    echo "<div style='display:none;' id='data'>".json_encode( $current )."</div>";
-    file_put_contents( './reviews.json', json_encode( $current ) );
+    echo "<div style='display:none;' id='data'>".$current."</div>";
+    file_put_contents( './reviews.json', $current );
   }
   else {
     file_put_contents( './reviews.json', "" );
   }
 ?>
 
+<!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="UTF-8">
