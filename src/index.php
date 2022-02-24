d26400a57a891013f024c71d5beb4e4573189e24
gobrent21@gmail.com <gobrent21@gmail.com>
2022-02-23 19:08:54 -0600

    feat: adjust encoding

--- src/index.php
+++ src/index.php
@@ -1,23 +1,24 @@
+<!DOCTYPE html>
 
 <?php
   // open file
-  $file = 'reviews.json';
+  $file = './reviews.json';
 
   // append to previous data
   if( file_exists( $file ) ) { // if previous data exists
     $current = file_get_contents( $file );
-    $current = JSON_Encode( $current );
+    $current = json_decode( $current );
 
     $current = str_replace( array( 'ghastly', 'repulsive', 'appalling', 'horrendous' ), '#bad', $current );
 
-    echo "<div style='display:none;' id='data'>".json_decode( $current )."</div>";
+    echo "<div style='display:none;' id='data'>".json_encode( $current )."</div>";
+    file_put_contents( './reviews.json', json_encode( $current ) );
   }
   else {
-    file_put_contents( 'reviews.json', '' );
+    file_put_contents( './reviews.json', "" );
   }
 ?>
 
-<!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="UTF-8">
